a554d17138e304d1f44b488f87b923517f4ac8a4
romahawk <romazuryk@gmail.com>
2025-05-05 16:49:05 +0200

    Delete firebase_config.py
    Deleted firebase_config.py

--- firebase_config.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
